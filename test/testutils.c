2c96d11bc2e19a241020bee517eafc337f71a441
MaskedW <maskedw00@gmail.com>
2016-05-06 09:23:28 +0900

    [test] xfsのマウントファイルシステムをfatfsとramfsで切り替えられるように変更
    テストでエラーになった時に、ramfsのエラーなのか、テストモジュールのエラーなのか
    がわかりづらいので、何かあった時は安心感のあるfatfsを使用した方がわかりやすい。

--- test/testutils.c
+++ test/testutils.c
@@ -6,8 +6,21 @@
 #include <ff.h>
 
 jmp_buf     g_xjmp;
+
+#define FS_TYPE_RAMfS   0
+#define FS_TYPE_FATFS   1
+#define FS_TYPE         FS_TYPE_RAMfS
+// #define FS_TYPE         FS_TYPE_RAMfS
+
+
+#if FS_TYPE == FS_TYPE_RAMfS
 static XRamFs* ramfs;
-static XVirtualFs* vramfs;
+#else
+static XFatFs* xfatfs;
+static FATFS* fatfs;
+void disk_deinit(BYTE pdrv);
+#endif
+static XVirtualFs* vfs;
 
 
 void x_escape_assertion_failed(const char* expr, const char* msg, const char* func, const char* file, int line)
@@ -38,19 +51,34 @@ void x_init_test(void)
 
 void x_test_init_fs()
 {
+    vfs = x_malloc(sizeof(*vfs));
+    X_ASSERT(vfs);
+
+#if FS_TYPE == FS_TYPE_RAMfS
     ramfs = x_malloc(sizeof(*ramfs));
-    vramfs = x_malloc(sizeof(*vramfs));
     X_ASSERT(ramfs);
-    X_ASSERT(vramfs);
 
     X_ASSERT(xramfs_init(ramfs, NULL, 10 * 1024) == X_ERR_NONE);
-    xramfs_init_vfs(ramfs, vramfs);
+    xramfs_init_vfs(ramfs, vfs);
+#else
+    xfatfs = malloc(sizeof(XFatFs));
+    fatfs = malloc(sizeof(FATFS));
+    X_ASSERT(xfatfs);
+    X_ASSERT(fatfs);
+    FRESULT res;
+    res = f_mount(fatfs, "0:", 0);
+    X_ASSERT(res == FR_OK);
+    res = f_mkfs("0:", 0, 0);
+    X_ASSERT(res == FR_OK);
+    xfatfs_init(xfatfs);
+    xfatfs_init_vfs(xfatfs, vfs);
+#endif
 
 #if X_XFS_TYPE == X_XFS_TYPE_UNION_FS
     xunionfs_init();
-    X_ASSERT(xunionfs_mount(vramfs, "/", "/") == X_ERR_NONE);
+    X_ASSERT(xunionfs_mount(vfs, "/", "/") == X_ERR_NONE);
 #else
-    xsinglefs_init(vramfs);
+    xsinglefs_init(vfs);
 #endif
 }
 
@@ -58,9 +86,18 @@ void x_test_init_fs()
 void x_test_deinit_fs()
 {
     xfs_deinit();
+    x_free(vfs);
+
+#if FS_TYPE == FS_TYPE_RAMfS
     xramfs_deinit(ramfs);
-    x_free(vramfs);
     x_free(ramfs);
+#else
+    xfatfs_deinit(xfatfs);
+    f_mount(NULL, "0:", 0);
+    free(fatfs);
+    free(xfatfs);
+    disk_deinit(0);
+#endif
 }
 
 
