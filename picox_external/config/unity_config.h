e2a8a31f81c59522930e9644be89a7e976cd6896
MaskedW <maskedw00@gmail.com>
2016-11-23 20:15:41 +0900

    <ext/Unity> malloc, freeをデフォルトでx_malloc, x_freeを使用するようにした

--- picox_external/config/unity_config.h
+++ picox_external/config/unity_config.h
@@ -9,6 +9,14 @@
     #define UNITY_OUTPUT_CHAR(c)    x_putc(c)
 #endif
 
+#ifndef UNITY_FIXTURE_MALLOC
+    #define UNITY_FIXTURE_MALLOC x_malloc
+#endif
+
+#ifndef UNITY_FIXTURE_FREE
+    #define UNITY_FIXTURE_FREE x_free
+#endif
+
 /* #define UNITY_EXCLUDE_FLOAT */
 /* #define UNITY_EXCLUDE_DOUBLE */
 
