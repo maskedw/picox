5abe4d0c262cda496082f8917d91bd803b97f889
MaskedW <maskedw00@gmail.com>
2017-01-18 21:48:30 +0900

    <test> Fix typo

--- tests/config/picox_config.h
+++ tests/config/picox_config.h
@@ -2,7 +2,8 @@
 #define picox_config_h_
 
 
-#define X_CONFIG_USE_FLOATING_POINT_PRINTF  (1)
+
+#define X_CONF_USE_FLOATING_POINT_PRINTF    (1)
 #define X_CONF_HAS_C99_MATH                 (1)
 #define X_CONF_XFS_TYPE                     (X_XFS_TYPE_UNION_FS)
 // #define X_CONF_XFS_TYPE                     (X_XFS_TYPE_SINGLE_FS)
